edfef8dd6e571cf6b392f1b13a66d214e3ddba21
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-03 00:05:56 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -6,8 +6,9 @@ export default function App() {
   <h1>إدارة المزارع الذكية</h1>
   <p>أهلاً بك في المشروع</p>
 
-  <button>إضافة مزارع</button>
-</div>
-    </div>
-  );
+<div><input type="text" placeholder="اسم المزارع" />
+ <button>إضافة مزارع</button>   </div>
+ </div>
+</div> );
 }
+/
