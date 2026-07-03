11283397042ac4c570b8fecae669c0d7f7e3afd3
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-03 15:27:55 +0300

    Delete srcsrc directory

--- srcsrc/pages/pages/Farmers.jsx
+++
@@ -1,7 +0,0 @@
-export default function Farmers() {
-  return (
-    <div>
-      <h1>إدارة المزارعين</h1>
-    </div>
-  );
-}
--- srcsrc/pages/pages/pages/Farms.jsx
+++
@@ -1,7 +0,0 @@
-export default function Farms() {
-  return (
-    <div>
-      <h1>إدارة المزارع</h1>
-    </div>
-  );
-}
