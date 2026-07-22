5af98fc707b7be90654a4fc4f7306de196a2efb3
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-22 23:20:05 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 export default function Header() {
   return (
     <header>
-      <h1>Smart Farm Management</h1>
+
+      <h1>
+        🌱 Smart Farm Management
+      </h1>
+
+      <p>
+        نظام إدارة المزارع الذكية
+      </p>
+
     </header>
   );
 }
