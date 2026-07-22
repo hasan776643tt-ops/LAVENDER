4dba601cf5786250a21afc0a9fe81797bd1f122d
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-22 19:54:00 +0300

    Update Map.jsx

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,30 @@
 export default function Map() {
   return (
     <div>
-      <h1>Map</h1>
+      <h1>📍 خريطة المزارع</h1>
+
+      <p>
+        هذه الصفحة مخصصة لعرض مواقع المزارع والحقول على الخريطة.
+      </p>
+
+      <div
+        style={{
+          width: "100%",
+          height: "300px",
+          border: "2px solid gray",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        🗺️ سيتم عرض الخريطة هنا لاحقاً
+      </div>
+
+      <br />
+
+      <button>
+        إضافة موقع مزرعة
+      </button>
     </div>
   );
 }
