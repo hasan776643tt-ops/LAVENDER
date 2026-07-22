7160e78827e646e1f92d40d259fd1879378de122
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-22 19:38:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 
+import Header from "./components/Header";
+
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
@@ -16,35 +18,61 @@ import Expenses from "./pages/Expenses";
 export default function App() {
   return (
     <div>
+
+      <Header />
+
       <nav>
         <Link to="/">الرئيسية</Link>
-        {" | "}
-        <Link to="/services">الخدمات</Link>
-        {" | "}
-        <Link to="/contact">تواصل معنا</Link>
         {" | "}
         <Link to="/farms">المزارع</Link>
         {" | "}
         <Link to="/fields">الحقول</Link>
         {" | "}
         <Link to="/crops">المحاصيل</Link>
+        {" | "}
+        <Link to="/weather">الطقس</Link>
+        {" | "}
+        <Link to="/map">الخريطة</Link>
+        {" | "}
+        <Link to="/expenses">المصاريف</Link>
+        {" | "}
+        <Link to="/services">الخدمات</Link>
+        {" | "}
+        <Link to="/contact">تواصل معنا</Link>
+        {" | "}
+        <Link to="/login">دخول</Link>
+        {" | "}
+        <Link to="/register">تسجيل</Link>
       </nav>
 
       <hr />
 
       <Routes>
+
         <Route path="/" element={<Home />} />
+
         <Route path="/services" element={<Services />} />
+
         <Route path="/contact" element={<Contact />} />
+
         <Route path="/farms" element={<Farms />} />
+
         <Route path="/fields" element={<Fields />} />
+
         <Route path="/crops" element={<Crops />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+
         <Route path="/weather" element={<Weather />} />
+
         <Route path="/map" element={<Map />} />
+
         <Route path="/expenses" element={<Expenses />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
       </Routes>
+
     </div>
   );
 }
