e611c87f74d08c1753d08bffb2f4b5cdc9444d31
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-22 23:13:44 +0300

    Update Users.jsx

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -2,79 +2,204 @@
 import { useState } from "react";
 
 export default function Users() {
+
   const [name, setName] = useState("");
+
   const [email, setEmail] = useState("");
+
   const [role, setRole] = useState("مزارع");
+
+
   const [users, setUsers] = useState([]);
 
+
+
   const addUser = () => {
+
     if (!name || !email) return;
 
+
     const newUser = {
-      name,
-      email,
-      role,
+
+      id: Date.now(),
+
+      name: name,
+
+      email: email,
+
+      role: role,
+
     };
 
-    setUsers([...users, newUser]);
+
+    setUsers([
+      ...users,
+      newUser
+    ]);
+
 
     setName("");
+
     setEmail("");
+
     setRole("مزارع");
+
   };
 
+
+
+  const deleteUser = (id) => {
+
+    const updatedUsers =
+      users.filter(
+        (user)=> user.id !== id
+      );
+
+
+    setUsers(updatedUsers);
+
+  };
+
+
+
   return (
+
     <div>
-      <h1>👤 إدارة المستخدمين</h1>
+
+
+      <h1>
+        👤 إدارة المستخدمين
+      </h1>
+
+
 
       <input
+
         type="text"
+
         placeholder="اسم المستخدم"
+
         value={name}
-        onChange={(e) => setName(e.target.value)}
+
+        onChange={(e)=>setName(e.target.value)}
+
       />
 
-      <br />
-      <br />
+
+
+      <br /><br />
+
+
 
       <input
+
         type="email"
+
         placeholder="البريد الإلكتروني"
+
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+
+        onChange={(e)=>setEmail(e.target.value)}
+
       />
 
-      <br />
-      <br />
+
+
+      <br /><br />
+
+
 
       <select
+
         value={role}
-        onChange={(e) => setRole(e.target.value)}
+
+        onChange={(e)=>setRole(e.target.value)}
+
       >
+
         <option>مزارع</option>
+
         <option>مهندس زراعي</option>
+
         <option>مشرف</option>
+
         <option>مدير النظام</option>
+
+
       </select>
 
-      <br />
-      <br />
+
+
+      <br /><br />
+
+
 
       <button onClick={addUser}>
+
         إضافة مستخدم
+
       </button>
+
+
 
       <hr />
 
-      <h2>قائمة المستخدمين</h2>
+
+
+      <h2>
+        قائمة المستخدمين
+      </h2>
+
+
 
       <ul>
-        {users.map((user, index) => (
-          <li key={index}>
-            {user.name} - {user.email} - {user.role}
+
+
+        {users.map((user)=>(
+
+
+          <li key={user.id}>
+
+
+            👤 {user.name}
+
+            <br />
+
+            📧 {user.email}
+
+            <br />
+
+            🔑 الصلاحية: {user.role}
+
+
+
+            <br />
+
+
+            <button
+
+              onClick={()=>deleteUser(user.id)}
+
+            >
+
+              حذف
+
+            </button>
+
+
+
           </li>
+
+
         ))}
+
+
       </ul>
+
+
+
     </div>
+
   );
+
 }
