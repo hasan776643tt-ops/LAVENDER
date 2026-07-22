2575356e8dcdf8877193161f75c64688e102089a
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-22 19:48:44 +0300

    Update Fields.jsx

--- src/pages/Fields.jsx
+++ src/pages/Fields.jsx
@@ -1,7 +1,53 @@
 export default function Fields() {
   return (
     <div>
-      <h1>Fields</h1>
+      <h1>🌱 إدارة الحقول</h1>
+
+      <p>
+        من هنا يمكن للمزارع إضافة ومتابعة حقول المزرعة.
+      </p>
+
+      <h2>إضافة حقل جديد</h2>
+
+      <label>اسم الحقل</label>
+      <br />
+      <input
+        type="text"
+        placeholder="اكتب اسم الحقل"
+      />
+
+      <br /><br />
+
+      <label>المحصول</label>
+      <br />
+      <input
+        type="text"
+        placeholder="نوع المحصول"
+      />
+
+      <br /><br />
+
+      <label>مساحة الحقل</label>
+      <br />
+      <input
+        type="number"
+        placeholder="بالهكتار أو المتر"
+      />
+
+      <br /><br />
+
+      <label>تاريخ الزراعة</label>
+      <br />
+      <input
+        type="date"
+      />
+
+      <br /><br />
+
+      <button>
+        حفظ الحقل
+      </button>
+
     </div>
   );
 }
