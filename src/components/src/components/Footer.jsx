4f8e60a814e10d7f1698b5fed806e2f69c88928b
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-23 15:44:00 +0300

    Create Footer.jsx

---
+++ src/components/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer>
+
+      <hr />
+
+      <p>
+        🌱 Smart Farm Management
+      </p>
+
+      <p>
+        نظام إدارة المزارع الذكية
+      </p>
+
+      <p>
+        جميع الحقوق محفوظة © 2026
+      </p>
+
+    </footer>
+  );
+}
