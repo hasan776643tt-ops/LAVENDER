49877f3080e073f88130bc1c00f181ca13379d19
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-23 15:46:49 +0300

    Create Button.jsx

---
+++ src/components/src/components/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ children, onClick, type = "button" }) {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
