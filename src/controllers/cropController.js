fbd12c9beb395c260e93bdbc3a2ce8be11a0724b
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-24 23:39:18 +0300

    Update cropController.js

--- src/controllers/cropController.js
+++ src/controllers/cropController.js
@@ -1,1 +1,25 @@
+import { cropRepository } from "../repositories/cropRepository";
 
+class CropController {
+  getCrops() {
+    return cropRepository.getAll();
+  }
+
+  getCropById(id) {
+    return cropRepository.getById(id);
+  }
+
+  createCrop(crop) {
+    return cropRepository.create(crop);
+  }
+
+  updateCrop(id, data) {
+    return cropRepository.update(id, data);
+  }
+
+  deleteCrop(id) {
+    return cropRepository.delete(id);
+  }
+}
+
+export const cropController = new CropController();
