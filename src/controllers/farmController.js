679e7a105ad8389dd7a6f492b3fafdd173fa9a2c
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-24 23:42:33 +0300

    Update farmController.js

--- src/controllers/farmController.js
+++ src/controllers/farmController.js
@@ -1,1 +1,25 @@
+import { farmRepository } from "../repositories/farmRepository";
 
+class FarmController {
+  getFarms() {
+    return farmRepository.getAll();
+  }
+
+  getFarmById(id) {
+    return farmRepository.getById(id);
+  }
+
+  createFarm(farm) {
+    return farmRepository.create(farm);
+  }
+
+  updateFarm(id, data) {
+    return farmRepository.update(id, data);
+  }
+
+  deleteFarm(id) {
+    return farmRepository.delete(id);
+  }
+}
+
+export const farmController = new FarmController();
