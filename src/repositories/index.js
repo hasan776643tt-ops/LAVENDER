0757ba4bef017b3d2c1d8198521d59da8e7146a8
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-24 23:19:26 +0300

    Create index.js

---
+++ src/repositories/index.js
@@ -0,0 +1,5 @@
+export { farmRepository } from "./farmRepository";
+export { fieldRepository } from "./fieldRepository";
+export { cropRepository } from "./cropRepository";
+export { userRepository } from "./userRepository";
+export { reportRepository } from "./reportRepository";
