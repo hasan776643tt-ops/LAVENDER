4879ab1bd899ada628886cbf310b3b5181043d0e
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-25 16:16:47 +0300

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+export default function Sidebar() {
+  return (
+    <aside>
+
+      <nav>
+
+        <ul>
+
+          <li>الرئيسية</li>
+
+          <li>المزارع</li>
+
+          <li>الحقول</li>
+
+          <li>المحاصيل</li>
+
+          <li>الري</li>
+
+          <li>الأسمدة</li>
+
+          <li>المبيدات</li>
+
+          <li>الأمراض</li>
+
+          <li>التقارير</li>
+
+        </ul>
+
+      </nav>
+
+    </aside>
+  );
+}
