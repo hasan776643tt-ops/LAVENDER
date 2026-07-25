e1cd0eb86ec4f0d8674bab589614415150999108
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-25 15:34:39 +0300

    Update fieldRoutes.js

--- src/routes/fieldRoutes.js
+++ src/routes/fieldRoutes.js
@@ -1,1 +1,35 @@
+import express from "express";
 
+import {
+  getFields,
+  getFieldById,
+  createField,
+  updateField,
+  deleteField
+} from "../controllers/fieldController.js";
+
+
+const router = express.Router();
+
+
+// جلب كل الحقول
+router.get("/", getFields);
+
+
+// جلب حقل واحد
+router.get("/:id", getFieldById);
+
+
+// إنشاء حقل
+router.post("/", createField);
+
+
+// تعديل حقل
+router.put("/:id", updateField);
+
+
+// حذف حقل
+router.delete("/:id", deleteField);
+
+
+export default router;
