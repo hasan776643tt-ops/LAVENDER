a774ef9b2fa0d44ad789564f16db665c2cf3b8e2
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-25 15:49:13 +0300

    Create index.js

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import userRoutes from "./userRoutes.js";
+import farmRoutes from "./farmRoutes.js";
+import fieldRoutes from "./fieldRoutes.js";
+import cropRoutes from "./cropRoutes.js";
+
+
+export {
+  userRoutes,
+  farmRoutes,
+  fieldRoutes,
+  cropRoutes
+};
