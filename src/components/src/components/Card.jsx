ce925bdf00db4324bcdd962a0424e094021003cb
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-25 18:57:51 +0300

    Create Card.jsx

---
+++ src/components/src/components/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card({ title, children }) {
+  return (
+    <div>
+
+      <h2>
+        {title}
+      </h2>
+
+      <div>
+        {children}
+      </div>
+
+    </div>
+  );
+}
