7dbd4acddcd14722131bd61dc2cb19e4c5aa490f
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-27 15:35:56 +0300

    Create countries.js

---
+++ src/config/countries.js
@@ -0,0 +1,45 @@
+// الدول المدعومة في نظام LAVENDER Smart Farm
+
+export const countries = [
+  {
+    code: "SY",
+    name: "سوريا",
+    currency: "SYP",
+    areaUnit: "dunum",
+  },
+
+  {
+    code: "TR",
+    name: "تركيا",
+    currency: "TRY",
+    areaUnit: "hectare",
+  },
+
+  {
+    code: "SA",
+    name: "السعودية",
+    currency: "SAR",
+    areaUnit: "hectare",
+  },
+
+  {
+    code: "AE",
+    name: "الإمارات",
+    currency: "AED",
+    areaUnit: "hectare",
+  },
+
+  {
+    code: "US",
+    name: "الولايات المتحدة",
+    currency: "USD",
+    areaUnit: "acre",
+  },
+
+  {
+    code: "FR",
+    name: "فرنسا",
+    currency: "EUR",
+    areaUnit: "hectare",
+  },
+];
