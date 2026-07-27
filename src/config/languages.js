d48497a6579d1faa6460c37b7ff7bb3ededc775d
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-27 15:32:12 +0300

    Create languages.js

---
+++ src/config/languages.js
@@ -0,0 +1,21 @@
+// اللغات المدعومة في نظام LAVENDER Smart Farm
+
+export const languages = [
+  {
+    code: "ar",
+    name: "العربية",
+    direction: "rtl",
+  },
+
+  {
+    code: "en",
+    name: "English",
+    direction: "ltr",
+  },
+
+  {
+    code: "tr",
+    name: "Türkçe",
+    direction: "ltr",
+  },
+];
