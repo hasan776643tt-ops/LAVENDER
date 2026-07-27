eb62191e07f4b06d701fcf5cc12254344947583e
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-27 15:34:55 +0300

    Create units.js

---
+++ src/config/units.js
@@ -0,0 +1,51 @@
+// وحدات القياس المدعومة في نظام LAVENDER Smart Farm
+
+export const units = {
+  area: [
+    {
+      code: "dunum",
+      name: "دونم",
+    },
+    {
+      code: "hectare",
+      name: "هكتار",
+    },
+    {
+      code: "acre",
+      name: "فدان",
+    },
+    {
+      code: "sqm",
+      name: "متر مربع",
+    },
+  ],
+
+  weight: [
+    {
+      code: "kg",
+      name: "كيلوغرام",
+    },
+    {
+      code: "ton",
+      name: "طن",
+    },
+  ],
+
+  water: [
+    {
+      code: "liter",
+      name: "لتر",
+    },
+    {
+      code: "cubic_meter",
+      name: "متر مكعب",
+    },
+  ],
+
+  volume: [
+    {
+      code: "liter",
+      name: "لتر",
+    },
+  ],
+};
