95e6bd96fdc33fb1a52719fa4d2df7d36c4e4666
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-27 15:33:41 +0300

    Create currencies.js

---
+++ src/config/currencies.js
@@ -0,0 +1,39 @@
+// العملات المدعومة في نظام LAVENDER Smart Farm
+
+export const currencies = [
+  {
+    code: "SYP",
+    name: "الليرة السورية",
+    symbol: "ل.س",
+  },
+
+  {
+    code: "TRY",
+    name: "الليرة التركية",
+    symbol: "₺",
+  },
+
+  {
+    code: "SAR",
+    name: "الريال السعودي",
+    symbol: "ر.س",
+  },
+
+  {
+    code: "AED",
+    name: "الدرهم الإماراتي",
+    symbol: "د.إ",
+  },
+
+  {
+    code: "USD",
+    name: "الدولار الأمريكي",
+    symbol: "$",
+  },
+
+  {
+    code: "EUR",
+    name: "اليورو",
+    symbol: "€",
+  },
+];
