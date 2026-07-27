db3c91277de7d5b015343c92fde33892eaae2c9a
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-27 22:39:51 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
@@ -8,23 +9,24 @@ import { SettingsProvider } from "./context/SettingsContext.jsx";
 
 import "./App.css";
 
-
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-
   <React.StrictMode>
 
-    <SettingsProvider>
+    <BrowserRouter>
 
-      <FarmProvider>
+      <SettingsProvider>
 
-        <App />
+        <FarmProvider>
 
-      </FarmProvider>
+          <App />
 
-    </SettingsProvider>
+        </FarmProvider>
+
+      </SettingsProvider>
+
+    </BrowserRouter>
 
   </React.StrictMode>
-
 );
