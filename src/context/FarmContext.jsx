5a408060573f49ae26146c6eefcf66cf1b26952d
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-28 14:00:56 +0300

    Update FarmContext.jsx

--- src/context/FarmContext.jsx
+++ src/context/FarmContext.jsx
@@ -1,109 +1,200 @@
-/* OTHER CRUD */
+/* =========================
+   AGRICULTURE CRUD SYSTEM
+========================= */
 
 
-addIrrigation:
-(data)=>
-addRecord(
-setIrrigations,
-data
-),
+// Irrigation
 
-updateIrrigation:
-(id,data)=>
-updateRecord(
-setIrrigations,
-id,
-data
-),
-
-deleteIrrigation:
-(id)=>
-deleteRecord(
-setIrrigations,
-id
-),
+const addIrrigation = (data) =>
+  addRecord(
+    setIrrigations,
+    data
+  );
 
 
+const updateIrrigation = (
+  id,
+  data
+) =>
+  updateRecord(
+    setIrrigations,
+    id,
+    data
+  );
 
-addFertilizer:
-(data)=>
-addRecord(
-setFertilizers,
-data
-),
 
-updateFertilizer:
-(id,data)=>
-updateRecord(
-setFertilizers,
-id,
-data
-),
-
-deleteFertilizer:
-(id)=>
-deleteRecord(
-setFertilizers,
-id
-),
-  addPesticide:
-(data)=>
-addRecord(
-setPesticides,
-data
-),
-
-updatePesticide:
-(id,data)=>
-updateRecord(
-setPesticides,
-id,
-data
-),
-
-deletePesticide:
-(id)=>
-deleteRecord(
-setPesticides,
-id
-),
+const deleteIrrigation = (id) =>
+  deleteRecord(
+    setIrrigations,
+    id
+  );
 
 
 
-addDisease:
-(data)=>
-addRecord(
-setDiseases,
-data
-),
 
-updateDisease:
-(id,data)=>
-updateRecord(
-setDiseases,
-id,
-data
-),
+// Fertilizers
 
-deleteDisease:
-(id)=>
-deleteRecord(
-setDiseases,
-id
-),
+const addFertilizer = (data) =>
+  addRecord(
+    setFertilizers,
+    data
+  );
 
 
+const updateFertilizer = (
+  id,
+  data
+) =>
+  updateRecord(
+    setFertilizers,
+    id,
+    data
+  );
 
-addExpense:
-(data)=>
-addRecord(
-setExpenses,
-data
-),
 
-deleteExpense:
-(id)=>
-deleteRecord(
-setExpenses,
-id
-),
+const deleteFertilizer = (id) =>
+  deleteRecord(
+    setFertilizers,
+    id
+  );
+
+
+
+
+// Pesticides
+
+const addPesticide = (data) =>
+  addRecord(
+    setPesticides,
+    data
+  );
+
+
+const updatePesticide = (
+  id,
+  data
+) =>
+  updateRecord(
+    setPesticides,
+    id,
+    data
+  );
+
+
+const deletePesticide = (id) =>
+  deleteRecord(
+    setPesticides,
+    id
+  );  // Diseases
+
+
+const addDisease = (data) =>
+  addRecord(
+    setDiseases,
+    data
+  );
+
+
+const updateDisease = (
+  id,
+  data
+) =>
+  updateRecord(
+    setDiseases,
+    id,
+    data
+  );
+
+
+const deleteDisease = (id) =>
+  deleteRecord(
+    setDiseases,
+    id
+  );
+
+
+
+
+// Expenses
+
+
+const addExpense = (data) =>
+  addRecord(
+    setExpenses,
+    data
+  );
+
+
+const updateExpense = (
+  id,
+  data
+) =>
+  updateRecord(
+    setExpenses,
+    id,
+    data
+  );
+
+
+const deleteExpense = (id) =>
+  deleteRecord(
+    setExpenses,
+    id
+  );
+
+
+
+
+// Future Agriculture Systems Ready
+
+
+const addHarvest = (data) =>
+  addRecord(
+    setHarvests,
+    data
+  );
+
+
+const updateHarvest = (
+  id,
+  data
+) =>
+  updateRecord(
+    setHarvests,
+    id,
+    data
+  );
+
+
+const deleteHarvest = (id) =>
+  deleteRecord(
+    setHarvests,
+    id
+  );
+
+
+
+
+const addInventory = (data) =>
+  addRecord(
+    setInventory,
+    data
+  );
+
+
+const updateInventory = (
+  id,
+  data
+) =>
+  updateRecord(
+    setInventory,
+    id,
+    data
+  );
+
+
+const deleteInventory = (id) =>
+  deleteRecord(
+    setInventory,
+    id
+  );
