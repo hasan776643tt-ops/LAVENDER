c146dcee65b136c1bebfd4f2578471014433414d
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-29 16:24:01 +0300

    Update userRepository.js

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,28 +1,168 @@
-import { userService } from "../api/userService";
+// src/repositories/userRepository.js
+
+import storageService
+  from "../services/storageService.js";
+
 
 class UserRepository {
-  getAll() {
-    return userService.getUsers();
+
+
+  constructor(){
+
+    this.key =
+      "users";
+
   }
 
-  getById(id) {
-    return this.getAll().find(user => user.id === id) || null;
+
+
+
+  getAll(){
+
+    return storageService.load(
+      this.key,
+      []
+    );
+
   }
 
-  create(user) {
-    userService.addUser(user);
+
+
+
+  getById(id){
+
+    return this.getAll().find(
+      user =>
+        user.id === id
+    ) || null;
+
+  }
+
+
+
+
+  create(user){
+
+    const users =
+      this.getAll();
+
+
+    users.push(
+      user
+    );
+
+
+    storageService.save(
+      this.key,
+      users
+    );
+
+
     return user;
+
   }
 
-  update(id, data) {
-    userService.updateUser(id, data);
-    return this.getById(id);
+
+
+
+  update(
+    id,
+    data
+  ){
+
+    const users =
+      this.getAll();
+
+
+    const index =
+      users.findIndex(
+        user =>
+          user.id === id
+      );
+
+
+    if(index === -1){
+
+      return null;
+
+    }
+
+
+    users[index] = {
+
+      ...users[index],
+
+      ...data,
+
+      updatedAt:
+        new Date().toISOString()
+
+    };
+
+
+    storageService.save(
+      this.key,
+      users
+    );
+
+
+    return users[index];
+
   }
 
-  delete(id) {
-    userService.deleteUser(id);
+
+
+
+  delete(id){
+
+    const users =
+      this.getAll();
+
+
+    const filtered =
+      users.filter(
+        user =>
+          user.id !== id
+      );
+
+
+    storageService.save(
+      this.key,
+      filtered
+    );
+
+
     return true;
+
   }
+
+
+
+
+  exists(id){
+
+    return this.getAll().some(
+      user =>
+        user.id === id
+    );
+
+  }
+
+
+
+
+  count(){
+
+    return this.getAll().length;
+
+  }
+
+
 }
 
-export const userRepository = new UserRepository();
+
+export const userRepository =
+  new UserRepository();
+
+
+export default userRepository;
