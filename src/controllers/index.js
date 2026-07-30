ebd7925ab37868ba62c2ef67546b17c6d50dc9da
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-30 12:19:17 +0300

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,25 +1,19 @@
 // src/controllers/index.js
 
-import {
-  cropController
-} from "./cropController.js";
-
-import {
-  farmController
-} from "./farmController.js";
-
-import {
-  fieldController
-} from "./fieldController.js";
-
-import {
-  userController
-} from "./userController.js";
+import cropController from "./cropController.js";
+import farmController from "./farmController.js";
+import fieldController from "./fieldController.js";
+import userController from "./userController.js";
+import fertilizerController from "./fertilizerController.js";
+import weatherController from "./weatherController.js";
+import reportController from "./reportController.js";
+import engineerController from "./engineerController.js";
+import diseaseController from "./diseaseController.js";
 
 
 // Central Controllers Registry
 
-export const controllers = {
+const controllers = {
 
   crop: cropController,
 
@@ -27,6 +21,19 @@ export const controllers = {
 
   field: fieldController,
 
-  user: userController
+  user: userController,
+
+  fertilizer: fertilizerController,
+
+  weather: weatherController,
+
+  report: reportController,
+
+  engineer: engineerController,
+
+  disease: diseaseController
 
 };
+
+
+export default Object.freeze(controllers);
