69edd2722180de6f5e16b494e0dc1e54cf21a735
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-30 13:39:11 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,33 @@
+// src/routes/index.js
+
+import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
 import farmRoutes from "./farmRoutes.js";
 import fieldRoutes from "./fieldRoutes.js";
 import cropRoutes from "./cropRoutes.js";
+import reportRoutes from "./reportRoutes.js";
+import engineerRoutes from "./engineerRoutes.js";
 
 
-export {
-  userRoutes,
-  farmRoutes,
-  fieldRoutes,
-  cropRoutes
+
+const routes = {
+
+  auth: authRoutes,
+
+  user: userRoutes,
+
+  farm: farmRoutes,
+
+  field: fieldRoutes,
+
+  crop: cropRoutes,
+
+  report: reportRoutes,
+
+  engineer: engineerRoutes
+
 };
+
+
+
+export default Object.freeze(routes);
