e01778d508c4cdbdc0e7cccaf6ca3ad70f754e82
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-30 16:41:37 +0300

    Update exportService.js

--- src/services/exportService.js
+++ src/services/exportService.js
@@ -1,23 +1,71 @@
+// src/services/exportService.js
+
+
 class ExportService {
+
+
   exportJSON(data) {
-    return JSON.stringify(data, null, 2);
+
+    return JSON.stringify(
+      data,
+      null,
+      2
+    );
+
   }
+
+
+
+
 
   importJSON(json) {
+
     try {
-      return JSON.parse(json);
+
+      return JSON.parse(
+        json
+      );
+
     } catch {
+
       return null;
+
     }
+
   }
+
+
+
+
 
   exportData(data) {
-    return this.exportJSON(data);
+
+    return this.exportJSON(
+      data
+    );
+
   }
+
+
+
+
 
   importData(data) {
-    return this.importJSON(data);
+
+    return this.importJSON(
+      data
+    );
+
   }
+
+
 }
 
-export const exportService = new ExportService();
+
+
+export const exportService =
+  new ExportService();
+
+
+
+export default exportService;
