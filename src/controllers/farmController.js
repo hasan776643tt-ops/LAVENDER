ddf7f1c8ae7a2384feda50d24a613ad7e07b91ee
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-30 12:05:26 +0300

    Update farmController.js

--- src/controllers/farmController.js
+++ src/controllers/farmController.js
@@ -1,25 +1,57 @@
-import { farmRepository } from "../repositories/farmRepository";
+import farmRepository from "../repositories/farmRepository.js";
 
 class FarmController {
-  getFarms() {
-    return farmRepository.getAll();
+
+  constructor() {
+    this.repository = farmRepository;
   }
 
-  getFarmById(id) {
-    return farmRepository.getById(id);
+
+  async getFarms() {
+    try {
+      return await this.repository.getAll();
+    } catch (error) {
+      throw new Error(`Failed to get farms: ${error.message}`);
+    }
   }
 
-  createFarm(farm) {
-    return farmRepository.create(farm);
+
+  async getFarmById(id) {
+    try {
+      return await this.repository.getById(id);
+    } catch (error) {
+      throw new Error(`Failed to get farm: ${error.message}`);
+    }
   }
 
-  updateFarm(id, data) {
-    return farmRepository.update(id, data);
+
+  async createFarm(farm) {
+    try {
+      return await this.repository.create(farm);
+    } catch (error) {
+      throw new Error(`Failed to create farm: ${error.message}`);
+    }
   }
 
-  deleteFarm(id) {
-    return farmRepository.delete(id);
+
+  async updateFarm(id, data) {
+    try {
+      return await this.repository.update(id, data);
+    } catch (error) {
+      throw new Error(`Failed to update farm: ${error.message}`);
+    }
   }
+
+
+  async deleteFarm(id) {
+    try {
+      return await this.repository.delete(id);
+    } catch (error) {
+      throw new Error(`Failed to delete farm: ${error.message}`);
+    }
+  }
+
 }
 
-export const farmController = new FarmController();
+
+export default Object.freeze(new FarmController());
