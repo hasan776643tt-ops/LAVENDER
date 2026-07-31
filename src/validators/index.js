f537305d740ec3ef167e093da2f453385e6cb096
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-31 15:11:20 +0300

    Update index.js

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,19 @@
+// src/validators/index.js
+
+
 export * from "./farmValidator";
+
 export * from "./fieldValidator";
+
 export * from "./cropValidator";
+
 export * from "./userValidator";
+
+
+export * from "./irrigationValidator";
+
+export * from "./pesticideValidator";
+
+export * from "./fertilizerValidator";
+
+export * from "./diseaseValidator";
