6d3964380cc7beae0bec18a8f46bbc16a4dab2b4
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-07-31 14:57:56 +0300

    Update index.js

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -21,9 +21,36 @@ export {
 } from "./userRepository.js";
 
 
-// مستقبلاً:
-// export { default as weatherRepository }
-// from "./weatherRepository.js";
-//
-// export { default as engineerRepository }
-// from "./engineerRepository.js";
+export {
+  default as irrigationRepository
+} from "./irrigationRepository.js";
+
+
+export {
+  default as fertilizerRepository
+} from "./fertilizerRepository.js";
+
+
+export {
+  default as pesticideRepository
+} from "./pesticideRepository.js";
+
+
+export {
+  default as diseaseRepository
+} from "./diseaseRepository.js";
+
+
+export {
+  default as weatherRepository
+} from "./weatherRepository.js";
+
+
+export {
+  default as engineerRepository
+} from "./engineerRepository.js";
+
+
+export {
+  default as reportRepository
+} from "./reportRepository.js";
