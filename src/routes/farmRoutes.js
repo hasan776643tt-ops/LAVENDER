3e929d218b63f360c38a924381b698f9bd83c60f
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 16:54:12 +0300

    Update farmRoutes.js

--- src/routes/farmRoutes.js
+++ src/routes/farmRoutes.js
@@ -1,214 +1,26 @@
 // src/routes/farmRoutes.js
 
 
-import farmController
-  from "../controllers/farmController.js";
+import {
+  lazy
+} from "react";
 
 
+const farmRoutes = [
 
+  {
+    path: "/farms",
 
-
-class FarmRoutes {
-
-
-
-  constructor(){
-
-
-    this.controller =
-      farmController;
-
+    element: lazy(
+      () =>
+        import("../pages/Farms.jsx")
+    )
 
   }
 
-
-
-
-
-
-
-
-  async getFarms(){
-
-
-    try{
-
-
-      return await this.controller.getFarms();
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `FarmRoutes getFarms failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async getFarmById(id){
-
-
-    try{
-
-
-      return await this.controller.getFarmById(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `FarmRoutes getFarmById failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async createFarm(data){
-
-
-    try{
-
-
-      return await this.controller.createFarm(
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `FarmRoutes createFarm failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async updateFarm(id,data){
-
-
-    try{
-
-
-      return await this.controller.updateFarm(
-
-        id,
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `FarmRoutes updateFarm failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async deleteFarm(id){
-
-
-    try{
-
-
-      return await this.controller.deleteFarm(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `FarmRoutes deleteFarm failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-}
-
-
-
+];
 
 
 export default Object.freeze(
-
-  new FarmRoutes()
-
+  farmRoutes
 );
