e50877f837cbc4b8e8e784221707230b27e23de1
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 16:52:44 +0300

    Update engineerRoutes.js

--- src/routes/engineerRoutes.js
+++ src/routes/engineerRoutes.js
@@ -1,250 +1,26 @@
 // src/routes/engineerRoutes.js
 
 
-import engineerController
-  from "../controllers/engineerController.js";
+import {
+  lazy
+} from "react";
 
 
+const engineerRoutes = [
 
+  {
+    path: "/engineer",
 
-
-class EngineerRoutes {
-
-
-
-  constructor(){
-
-
-    this.controller =
-      engineerController;
-
+    element: lazy(
+      () =>
+        import("../pages/Engineer.jsx")
+    )
 
   }
 
-
-
-
-
-
-
-
-  async getAll(){
-
-
-    try{
-
-
-      return await this.controller.getAllEngineers();
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `EngineerRoutes getAll failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async getById(id){
-
-
-    try{
-
-
-      return await this.controller.getEngineerById(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `EngineerRoutes getById failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async create(data){
-
-
-    try{
-
-
-      return await this.controller.createEngineer(
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `EngineerRoutes create failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async update(id,data){
-
-
-    try{
-
-
-      return await this.controller.updateEngineer(
-
-        id,
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `EngineerRoutes update failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async delete(id){
-
-
-    try{
-
-
-      return await this.controller.deleteEngineer(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `EngineerRoutes delete failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async search(keyword){
-
-
-    try{
-
-
-      return await this.controller.searchEngineers(
-
-        keyword
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `EngineerRoutes search failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-}
-
-
-
+];
 
 
 export default Object.freeze(
-
-  new EngineerRoutes()
-
+  engineerRoutes
 );
