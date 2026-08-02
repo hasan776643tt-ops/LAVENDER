e3b4fcc82e139c1c45d22ea79d653da105c5f4bf
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 16:41:28 +0300

    Update currencies.js

--- src/config/currencies.js
+++ src/config/currencies.js
@@ -1,39 +1,69 @@
-// العملات المدعومة في نظام LAVENDER Smart Farm
+// src/config/currencies.js
 
-export const currencies = [
-  {
+
+const currencies = Object.freeze([
+
+  Object.freeze({
+
     code: "SYP",
+
     name: "الليرة السورية",
-    symbol: "ل.س",
-  },
 
-  {
+    symbol: "ل.س"
+
+  }),
+
+  Object.freeze({
+
     code: "TRY",
+
     name: "الليرة التركية",
-    symbol: "₺",
-  },
 
-  {
+    symbol: "₺"
+
+  }),
+
+  Object.freeze({
+
     code: "SAR",
+
     name: "الريال السعودي",
-    symbol: "ر.س",
-  },
 
-  {
+    symbol: "ر.س"
+
+  }),
+
+  Object.freeze({
+
     code: "AED",
+
     name: "الدرهم الإماراتي",
-    symbol: "د.إ",
-  },
 
-  {
+    symbol: "د.إ"
+
+  }),
+
+  Object.freeze({
+
     code: "USD",
-    name: "الدولار الأمريكي",
-    symbol: "$",
-  },
 
-  {
+    name: "الدولار الأمريكي",
+
+    symbol: "$"
+
+  }),
+
+  Object.freeze({
+
     code: "EUR",
+
     name: "اليورو",
-    symbol: "€",
-  },
-];
+
+    symbol: "€"
+
+  })
+
+]);
+
+
+export default currencies;
