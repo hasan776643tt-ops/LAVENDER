0e04755d01e59765f2581befc7283e1189921955
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 16:50:36 +0300

    Update cropRoutes.js

--- src/routes/cropRoutes.js
+++ src/routes/cropRoutes.js
@@ -1,214 +1,26 @@
 // src/routes/cropRoutes.js
 
 
-import cropController
-  from "../controllers/cropController.js";
+import {
+  lazy
+} from "react";
 
 
+const cropRoutes = [
 
+  {
+    path: "/crops",
 
-
-class CropRoutes {
-
-
-
-  constructor(){
-
-
-    this.controller =
-      cropController;
-
+    element: lazy(
+      () =>
+        import("../pages/Crops.jsx")
+    )
 
   }
 
-
-
-
-
-
-
-
-  async getCrops(){
-
-
-    try{
-
-
-      return await this.controller.getCrops();
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `CropRoutes getCrops failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async getCropById(id){
-
-
-    try{
-
-
-      return await this.controller.getCropById(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `CropRoutes getCropById failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async createCrop(data){
-
-
-    try{
-
-
-      return await this.controller.createCrop(
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `CropRoutes createCrop failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async updateCrop(id,data){
-
-
-    try{
-
-
-      return await this.controller.updateCrop(
-
-        id,
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `CropRoutes updateCrop failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async deleteCrop(id){
-
-
-    try{
-
-
-      return await this.controller.deleteCrop(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `CropRoutes deleteCrop failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-}
-
-
-
+];
 
 
 export default Object.freeze(
-
-  new CropRoutes()
-
+  cropRoutes
 );
