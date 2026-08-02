507b4ec6ea068d068f14d96650e5562b55307908
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 17:00:09 +0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,214 +1,26 @@
 // src/routes/userRoutes.js
 
 
-import userController
-  from "../controllers/userController.js";
+import {
+  lazy
+} from "react";
 
 
+const userRoutes = [
 
+  {
+    path: "/users",
 
-
-class UserRoutes {
-
-
-
-  constructor(){
-
-
-    this.controller =
-      userController;
-
+    element: lazy(
+      () =>
+        import("../pages/Users.jsx")
+    )
 
   }
 
-
-
-
-
-
-
-
-  async getUsers(){
-
-
-    try{
-
-
-      return await this.controller.getUsers();
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `UserRoutes getUsers failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async getUserById(id){
-
-
-    try{
-
-
-      return await this.controller.getUserById(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `UserRoutes getUserById failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async createUser(data){
-
-
-    try{
-
-
-      return await this.controller.createUser(
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `UserRoutes createUser failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async updateUser(id,data){
-
-
-    try{
-
-
-      return await this.controller.updateUser(
-
-        id,
-
-        data
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `UserRoutes updateUser failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-
-
-
-
-
-  async deleteUser(id){
-
-
-    try{
-
-
-      return await this.controller.deleteUser(
-
-        id
-
-      );
-
-
-
-    }catch(error){
-
-
-      throw new Error(
-
-        `UserRoutes deleteUser failed: ${error.message}`
-
-      );
-
-
-    }
-
-
-  }
-
-
-
-}
-
-
-
+];
 
 
 export default Object.freeze(
-
-  new UserRoutes()
-
+  userRoutes
 );
