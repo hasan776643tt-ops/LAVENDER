33bc71b74a4bca8289eb689d752c691c9759e1ad
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 15:11:09 +0300

    Update farmController.js

--- src/controllers/farmController.js
+++ src/controllers/farmController.js
@@ -1,143 +1,77 @@
 // src/controllers/farmController.js
 
-import farmService
-from "../services/farmService.js";
-
+import farmService from "../services/farmService.js";
 
 
 class FarmController {
 
 
-constructor(){
+  constructor() {
 
- this.service =
- farmService;
+    this.service = farmService;
 
-}
-
+  }
 
 
+  async getAll() {
 
-async getAll(){
+    return this.service.getAll();
 
- try{
-
-  return await this.service.getAll();
-
- }catch(error){
-
-  throw new Error(
-   `FarmController getAll failed: ${error.message}`
-  );
-
- }
-
-}
+  }
 
 
+  async getById(id) {
+
+    return this.service.getById(id);
+
+  }
 
 
-async getById(id){
+  async create(data) {
 
- try{
+    return this.service.create(data);
 
-  return await this.service.getById(id);
-
- }catch(error){
-
-  throw new Error(
-   `FarmController getById failed: ${error.message}`
-  );
-
- }
-
-}
+  }
 
 
+  async update(id, data) {
+
+    return this.service.update(
+      id,
+      data
+    );
+
+  }
 
 
-async create(data){
+  async delete(id) {
 
- try{
+    return this.service.delete(id);
 
-  return await this.service.create(data);
-
- }catch(error){
-
-  throw new Error(
-   `FarmController create failed: ${error.message}`
-  );
-
- }
-
-}
+  }
 
 
+  async count() {
+
+    return this.service.count();
+
+  }
 
 
-async update(id,data){
+  async exists(id) {
 
- try{
+    return this.service.exists(id);
 
-  return await this.service.update(
-   id,
-   data
-  );
-
- }catch(error){
-
-  throw new Error(
-   `FarmController update failed: ${error.message}`
-  );
-
- }
-
-}
-
-
-
-
-async remove(id){
-
- try{
-
-  return await this.service.delete(id);
-
- }catch(error){
-
-  throw new Error(
-   `FarmController remove failed: ${error.message}`
-  );
-
- }
-
-}
-
-
-
-
-async count(){
-
- try{
-
-  return await this.service.count();
-
- }catch(error){
-
-  throw new Error(
-   `FarmController count failed: ${error.message}`
-  );
-
- }
-
-}
-
+  }
 
 
 }
 
+
+const farmController =
+new FarmController();
 
 
 export default Object.freeze(
- new FarmController()
+  farmController
 );
