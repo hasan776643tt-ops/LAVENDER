f2af8c31b3a2afbad7e2d55adae7e66c219b320c
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-02 22:31:05 +0300

    Update farmService.js

--- src/api/farmService.js
+++ src/api/farmService.js
@@ -1,26 +1,173 @@
-import { DataModel } from "../context/DataModel";
+// src/services/farmService.js
 
-export const farmService = {
-  getFarms() {
-    return DataModel.farms;
-  },
+import farmRepository
+  from "../repositories/farmRepository.js";
 
-  addFarm(farm) {
-    DataModel.farms.push(farm);
-  },
 
-  updateFarm(id, data) {
-    const index = DataModel.farms.findIndex(f => f.id === id);
+class FarmService {
 
-    if (index !== -1) {
-      DataModel.farms[index] = {
-        ...DataModel.farms[index],
-        ...data,
-      };
+
+  constructor() {
+
+    this.repository =
+      farmRepository;
+
+  }
+
+
+
+  async getAll() {
+
+    return await this.repository.getAll();
+
+  }
+
+
+
+  async getById(id) {
+
+
+    if (!id) {
+
+      throw new Error(
+        "Farm id is required"
+      );
+
     }
-  },
 
-  deleteFarm(id) {
-    DataModel.farms = DataModel.farms.filter(f => f.id !== id);
-  },
-};
+
+    return await this.repository.getById(id);
+
+  }
+
+
+
+  async create(data) {
+
+
+    if (!data) {
+
+      throw new Error(
+        "Farm data is required"
+      );
+
+    }
+
+
+    return await this.repository.create(data);
+
+  }
+
+
+
+  async update(id, data) {
+
+
+    if (!id) {
+
+      throw new Error(
+        "Farm id is required"
+      );
+
+    }
+
+
+    if (!data) {
+
+      throw new Error(
+        "Farm data is required"
+      );
+
+    }
+
+
+    const result =
+      await this.repository.update(
+        id,
+        data
+      );
+
+
+    if (!result) {
+
+      throw new Error(
+        "Farm not found"
+      );
+
+    }
+
+
+    return result;
+
+  }
+
+
+
+  async delete(id) {
+
+
+    if (!id) {
+
+      throw new Error(
+        "Farm id is required"
+      );
+
+    }
+
+
+    const result =
+      await this.repository.delete(id);
+
+
+    if (!result) {
+
+      throw new Error(
+        "Farm not found"
+      );
+
+    }
+
+
+    return true;
+
+  }
+
+
+
+  async count() {
+
+    return await this.repository.count();
+
+  }
+
+
+
+  async exists(id) {
+
+
+    if (!id) {
+
+      throw new Error(
+        "Farm id is required"
+      );
+
+    }
+
+
+    return await this.repository.exists(id);
+
+  }
+
+
+}
+
+
+
+const farmService =
+  new FarmService();
+
+
+
+export default Object.freeze(
+  farmService
+);
