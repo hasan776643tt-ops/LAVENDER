3a110d03dc1edf731a74dcdc3edcbea963d7eacd
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-03 16:35:06 +0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,65 @@
-export { Farm } from "./Farm";
-export { Field } from "./Field";
-export { Crop } from "./Crop";
-export { User } from "./User";
+// src/models/index.js
+
+
+export {
+  FarmModel,
+  createFarm
+} from "./FarmModel.js";
+
+
+export {
+  FieldModel,
+  createField
+} from "./FieldModel.js";
+
+
+export {
+  CropModel,
+  createCrop
+} from "./CropModel.js";
+
+
+export {
+  UserModel,
+  createUser
+} from "./UserModel.js";
+
+
+export {
+  DiseaseModel
+} from "./DiseaseModel.js";
+
+
+export {
+  EngineerModel
+} from "./EngineerModel.js";
+
+
+export {
+  ExpenseModel
+} from "./ExpenseModel.js";
+
+
+export {
+  FertilizerModel
+} from "./FertilizerModel.js";
+
+
+export {
+  IrrigationModel
+} from "./IrrigationModel.js";
+
+
+export {
+  PesticideModel
+} from "./PesticideModel.js";
+
+
+export {
+  ReportModel
+} from "./ReportModel.js";
+
+
+export {
+  ConsultationModel
+} from "./ConsultationModel.js";
