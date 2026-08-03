6410807f18525a04be4439612117a89b8def71f0
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-03 16:12:49 +0300

    Create index.js

---
+++ src/api/index.js
@@ -0,0 +1,66 @@
+// src/api/index.js
+
+
+export {
+  default as cropApi
+} from "./cropApi.js";
+
+
+export {
+  default as farmApi
+} from "./farmApi.js";
+
+
+export {
+  default as fieldApi
+} from "./fieldApi.js";
+
+
+export {
+  default as irrigationApi
+} from "./irrigationApi.js";
+
+
+export {
+  default as fertilizerApi
+} from "./fertilizerApi.js";
+
+
+export {
+  default as pesticideApi
+} from "./pesticideApi.js";
+
+
+export {
+  default as diseaseApi
+} from "./diseaseApi.js";
+
+
+export {
+  default as weatherApi
+} from "./weatherApi.js";
+
+
+export {
+  default as engineerApi
+} from "./engineerApi.js";
+
+
+export {
+  default as reportApi
+} from "./reportApi.js";
+
+
+export {
+  default as expenseApi
+} from "./expenseApi.js";
+
+
+export {
+  default as harvestApi
+} from "./harvestApi.js";
+
+
+export {
+  default as inventoryApi
+} from "./inventoryApi.js";
