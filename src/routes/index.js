271343350691a0191fb2f6dc491b87a0afdf2cc6
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-03 15:15:32 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,45 +2,90 @@
 
 
 import authRoutes from "./authRoutes.js";
-
 import userRoutes from "./userRoutes.js";
-
 import farmRoutes from "./farmRoutes.js";
-
 import fieldRoutes from "./fieldRoutes.js";
-
 import cropRoutes from "./cropRoutes.js";
-
 import reportRoutes from "./reportRoutes.js";
-
 import engineerRoutes from "./engineerRoutes.js";
-
 import weatherRoutes from "./weatherRoutes.js";
 
 
 
-const routes = [
+const routeGroups = Object.freeze([
 
-  ...authRoutes,
+  authRoutes,
 
-  ...userRoutes,
+  userRoutes,
 
-  ...farmRoutes,
+  farmRoutes,
 
-  ...fieldRoutes,
+  fieldRoutes,
 
-  ...cropRoutes,
+  cropRoutes,
 
-  ...reportRoutes,
+  reportRoutes,
 
-  ...engineerRoutes,
+  engineerRoutes,
 
-  ...weatherRoutes,
+  weatherRoutes
 
-];
+]);
 
 
 
-export default Object.freeze(
-  routes
+const routes = Object.freeze(
+
+  routeGroups.flat()
+
 );
+
+
+
+export const publicRoutes = Object.freeze(
+
+  routes.filter(
+
+    route => !route.protected
+
+  )
+
+);
+
+
+
+export const protectedRoutes = Object.freeze(
+
+  routes.filter(
+
+    route => route.protected
+
+  )
+
+);
+
+
+
+export const routeMap = Object.freeze(
+
+  Object.fromEntries(
+
+    routes.map(
+
+      route => [
+
+        route.id,
+
+        route
+
+      ]
+
+    )
+
+  )
+
+);
+
+
+
+export default routes;
