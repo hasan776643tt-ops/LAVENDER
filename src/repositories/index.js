8de2da68a24f1476d9c9968a9923b4f754321bff
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-03 19:19:21 +0300

    Update index.js

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -5,57 +5,71 @@ export {
   default as farmRepository
 } from "./farmRepository.js";
 
+
 export {
   default as fieldRepository
 } from "./fieldRepository.js";
+
 
 export {
   default as cropRepository
 } from "./cropRepository.js";
 
+
 export {
   default as userRepository
 } from "./userRepository.js";
+
 
 export {
   default as irrigationRepository
 } from "./irrigationRepository.js";
 
+
 export {
   default as fertilizerRepository
 } from "./fertilizerRepository.js";
+
 
 export {
   default as pesticideRepository
 } from "./pesticideRepository.js";
 
+
 export {
   default as diseaseRepository
 } from "./diseaseRepository.js";
+
 
 export {
   default as weatherRepository
 } from "./weatherRepository.js";
 
+
 export {
   default as engineerRepository
 } from "./engineerRepository.js";
+
 
 export {
   default as reportRepository
 } from "./reportRepository.js";
 
+
 export {
   default as expenseRepository
 } from "./expenseRepository.js";
+
 
 export {
   default as harvestRepository
 } from "./harvestRepository.js";
 
+
 export {
   default as inventoryRepository
 } from "./inventoryRepository.js";
+
 
 export {
   default as consultationRepository
