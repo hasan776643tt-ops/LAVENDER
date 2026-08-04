74a5b1ff8da4c8a70719b8135364fdfe1c12657a
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-04 16:59:29 +0300

    Update harvestController.js

--- src/controllers/harvestController.js
+++ src/controllers/harvestController.js
@@ -1,66 +1,163 @@
 // src/controllers/harvestController.js
 
-import harvestService from "../services/harvestService.js";
+
+import harvestService
+  from "../services/harvestService.js";
+
 
 
 class HarvestController {
 
 
+
   constructor() {
 
-    this.service = harvestService;
+    this.service =
+      harvestService;
 
   }
+
 
 
   async getAll() {
 
-    return this.service.getAll();
+    try {
+
+      return await this.service.getAll();
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_GET_ALL_FAILED:${error.message}`
+      );
+
+    }
 
   }
+
 
 
   async getById(id) {
 
-    return this.service.getById(id);
+    try {
+
+      return await this.service.getById(
+        id
+      );
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_GET_BY_ID_FAILED:${error.message}`
+      );
+
+    }
 
   }
+
 
 
   async create(data) {
 
-    return this.service.create(data);
+    try {
+
+      return await this.service.create(
+        data
+      );
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_CREATE_FAILED:${error.message}`
+      );
+
+    }
 
   }
+
 
 
   async update(id, data) {
 
-    return this.service.update(
-      id,
-      data
-    );
+    try {
+
+      return await this.service.update(
+        id,
+        data
+      );
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_UPDATE_FAILED:${error.message}`
+      );
+
+    }
 
   }
+
 
 
   async delete(id) {
 
-    return this.service.delete(id);
+    try {
+
+      return await this.service.delete(
+        id
+      );
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_DELETE_FAILED:${error.message}`
+      );
+
+    }
 
   }
 
-
-  async count() {
-
-    return this.service.count();
-
-  }
 
 
   async exists(id) {
 
-    return this.service.exists(id);
+    try {
+
+      return await this.service.exists(
+        id
+      );
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_EXISTS_FAILED:${error.message}`
+      );
+
+    }
+
+  }
+
+
+
+  async count() {
+
+    try {
+
+      return await this.service.count();
+
+
+    } catch(error) {
+
+      throw new Error(
+        `HARVEST_COUNT_FAILED:${error.message}`
+      );
+
+    }
 
   }
 
@@ -68,10 +165,7 @@ class HarvestController {
 }
 
 
-const harvestController =
-new HarvestController();
-
 
 export default Object.freeze(
-  harvestController
+  new HarvestController()
 );
