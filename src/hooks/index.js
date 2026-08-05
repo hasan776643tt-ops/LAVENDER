199acd40a0723e80758f0654b6f19ccbe5209f3c
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-05 12:57:40 +0300

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,17 +1,61 @@
 // src/hooks/index.js
 
 
+export { default as useAuth }
+from "./useAuth.js";
+
+
 export { default as useFarm }
 from "./useFarm.js";
+
 
 export { default as useFarms }
 from "./useFarms.js";
 
+
 export { default as useFields }
 from "./useFields.js";
+
 
 export { default as useCrops }
 from "./useCrops.js";
 
+
 export { default as useWeather }
 from "./useWeather.js";
+
+
+export { default as useLocalStorage }
+from "./useLocalStorage.js";
+
+
+export { default as useApi }
+from "./useApi.js";
+
+
+export { default as useFetch }
+from "./useFetch.js";
+
+
+export { default as useGeoLocation }
+from "./useGeoLocation.js";
+
+
+export { default as usePagination }
+from "./usePagination.js";
+
+
+export { default as useSearch }
+from "./useSearch.js";
+
+
+export { default as useFilter }
+from "./useFilter.js";
+
+
+export { default as useAnalytics }
+from "./useAnalytics.js";
+
+
+export { default as useAI }
+from "./useAI.js";
