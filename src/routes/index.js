e942ffd7fc3d5c37831c876cbedb41cf287f04ad
hasan776643tt-ops <hasan776643tt@gmail.com>
2026-08-05 22:36:45 +0300

    Add additional route imports to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,20 @@
 
 import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
+
 import farmRoutes from "./farmRoutes.js";
 import fieldRoutes from "./fieldRoutes.js";
 import cropRoutes from "./cropRoutes.js";
+
+import irrigationRoutes from "./irrigationRoutes.js";
+import fertilizerRoutes from "./fertilizerRoutes.js";
+import pesticideRoutes from "./pesticideRoutes.js";
+import diseaseRoutes from "./diseaseRoutes.js";
+
+import expenseRoutes from "./expenseRoutes.js";
+import harvestRoutes from "./harvestRoutes.js";
+import inventoryRoutes from "./inventoryRoutes.js";
+
 import reportRoutes from "./reportRoutes.js";
 import engineerRoutes from "./engineerRoutes.js";
 import weatherRoutes from "./weatherRoutes.js";
@@ -18,11 +29,29 @@ const routeGroups = Object.freeze([
 
   userRoutes,
 
+
   farmRoutes,
 
   fieldRoutes,
 
   cropRoutes,
+
+
+  irrigationRoutes,
+
+  fertilizerRoutes,
+
+  pesticideRoutes,
+
+  diseaseRoutes,
+
+
+  expenseRoutes,
+
+  harvestRoutes,
+
+  inventoryRoutes,
+
 
   reportRoutes,
 
